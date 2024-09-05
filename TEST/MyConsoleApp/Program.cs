6e0ee40f3e8d5926ea656ca844cb6969723ba226
HammerRoo <hammerroofficial@gmail.com>
2024-09-05 17:00:26 +0300

    Add files via upload

---
+++ TEST/MyConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Hello, World!");
+var currentDate = DateTime.Now;
+Console.WriteLine("The current time is:" + currentDate);
+Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+Console.WriteLine($"{Environment.NewLine} Hello {name}, on {currentDate}");
